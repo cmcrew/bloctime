5f8c9d4c5d4da3b7d93af60c1364a7052c62d053
Courtney Mitchell <courtney.mitchell@live.com>
2015-05-24 08:53:36 -0700

    display countdown timer on home screen

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,20 @@ blocTime.config(['$stateProvider', '$locationProvider', function($stateProvider,
   });
 }]);
 
-blocTime.controller('HomeController', ['$scope', function($scope) {
-  $scope.title = "Welcome to Bloc Time!"
+blocTime.controller('HomeController', ['$scope', '$interval', function($scope, $interval) {
+  $scope.title = "Welcome to Bloc Time!";
+  $scope.time = 65;
+  $scope.buttonLabel = "Start";
+  $scope.timeString = "" + $scope.time;
+  var countDown = function() {
+    $scope.time -= 1;
+    $scope.buttonLabel = "Reset";
+    if ($scope.time == 0) {
+      $interval.cancel(interval);
+    }
+    // See Bloc Jams Filter
+    $scope.timeString = parseInt($scope.time/60, 10) + ":" + ($scope.time % 60);
+  }
+
+  var interval = $interval(countDown, 1000);
 }]);
