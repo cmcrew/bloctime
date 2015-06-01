d9cb92e5b0da6427e4d4987ca407d02a4056f44c
Courtney Mitchell <courtney.mitchell@live.com>
2015-06-01 10:35:00 -0700

    update testing script

--- test/tests.js
+++ test/tests.js
@@ -1,15 +1,23 @@
-describe('HomeController', function(){
-  var $scope;
+// describe('HomeController', function(){
+//   var $scope;
 
+//   beforeEach(module('bloctime'));
+
+//   beforeEach(inject(function($rootScope, $controller, $interval, $filter) {
+//     $scope = $rootScope.$new();
+//     $controller('HomeController', {$scope: $scope, $interval: $interval, $filter: $filter});
+//   }));
+
+//   it('should decrement the time', function() {
+//     $scope.countDown();
+//     expect($scope.time).to.equal(24);
+//   });
+// })
+
+describe('remainingTimeFilter', function() {
   beforeEach(module('bloctime'));
-
-  beforeEach(inject(function($rootScope, $controller, $interval, $filter) {
-    $scope = $rootScope.$new();
-    $controller('HomeController', {$scope: $scope, $interval: $interval, $filter: $filter});
+  it('should convert milliseconds to minutes:seconds formatted time',
+    inject(function(remainingTimeFilter) {
+      expect(remainingTimeFilter(20)).to.equal("0:20");
   }));
-
-  it('should decrement the time', function() {
-    $scope.countDown();
-    expect($scope.time).to.equal(24);
-  });
 })
